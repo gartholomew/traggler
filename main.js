82fd04183d375081a0bc9991e830a5a45bd22bcb
James Brown <garthopotamus@gmail.com>
2020-12-15 10:54:08 -0700

    feat: add ReplayGain scanning

--- main.js
+++ main.js
@@ -12,6 +12,21 @@ function createWindow() {
 
     // and load the index.html of the app.
     win.loadFile('index.html')
+
+    const executablePath = 'mp3gain'; // v1.5.2 @ http://mp3gain.sourceforge.net/
+    const mp3gainArgs = [
+        '-k', // Automatically lower Track/Album gain to not clip audio
+        'test_album/*.mp3' // Files to analyze
+    ];
+    const child = require('child_process');
+    child.execFile(executablePath, mp3gainArgs, function (err, stdout, stderr) {
+        if (err) {
+            console.error(`err: ${err}`);
+            return;
+        }
+        console.log(`stdout: ${stdout.toString()}`);
+        console.log(`stderr: ${stderr.toString()}`);
+    });
 }
 
 app.whenReady().then(createWindow)
