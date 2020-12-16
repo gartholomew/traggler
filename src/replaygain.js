47634255a3f7ebca2fc3f92f2ddff20dc0088a96
James Brown <garthopotamus@gmail.com>
2020-12-16 10:36:08 -0700

    feat: integrate with @vue/cli and vue-cli-plugin-electron-builder

--- src/replaygain.js
+++ src/replaygain.js
@@ -1,22 +1,9 @@
-const {app, BrowserWindow} = require('electron')
-
-function createWindow() {
-    // Create the browser window.
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-
-    // and load the index.html of the app.
-    win.loadFile('index.html')
-
+/*
+function applyAlbumGain() {
     const executablePath = 'mp3gain'; // v1.5.2 @ http://mp3gain.sourceforge.net/
     const mp3gainArgs = [
         '-k', // Automatically lower Track/Album gain to not clip audio
-        'test_album/*.mp3' // Files to analyze
+        'test_album/!*.mp3' // Files to analyze
     ];
     const child = require('child_process');
     child.execFile(executablePath, mp3gainArgs, function (err, stdout, stderr) {
@@ -28,5 +15,4 @@ function createWindow() {
         console.log(`stderr: ${stderr.toString()}`);
     });
 }
-
-app.whenReady().then(createWindow)
+*/
