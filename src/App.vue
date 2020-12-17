f6a9777dd7a5126e819b12d3bb27ce567744aef7
James Brown <garthopotamus@gmail.com>
2020-12-17 14:32:07 -0700

    feat: read existing tags from album files and display in the app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <AlbumInput @albumSelected="onAlbumSelected"/>
-  <TagDisplay v-for="tag in tags" v-bind:tag-name="tag.display" v-bind:key="tag.id"/>
+  <TagDisplay v-for="tag in tags" v-bind:tag-name="tag.display" v-bind:tag-value="tag.value" v-bind:key="tag.id"/>
 </template>
 
 <script>
-import AlbumInput from "./components/AlbumInput.vue"
-import TagDisplay from "./components/TagDisplay.vue"
+import AlbumInput from "./components/AlbumInput.vue";
+import TagDisplay from "./components/TagDisplay.vue";
+
+const NodeID3 = require('node-id3');
 
 export default {
   name: "App",
@@ -14,18 +16,25 @@ export default {
   },
   data() {
     return {
-      tags: [
-        {id: "artist", display: "Artist Name"},
-        {id: "album", display: "Album Title"},
-        {id: "album-artist", display: "Album Artist"},
-        {id: "date", display: "Date"},
-        {id: "genre", display: "Genre"}
-      ]
+      tags: {
+        artist: {display: "Artist Name"},
+        album: {display: "Album Title"},
+        performerInfo: {display: "Album Artist"},
+        year: {display: "Date"},
+        partOfSet: {display: "Total Discs"},
+        genre: {display: "Genre"}
+      }
     }
   },
   methods: {
     onAlbumSelected(files) {
-      console.log(files);
+      const trackTags = [];
+      files.forEach(file => trackTags.push(NodeID3.read(file.path)));
+
+      for (let tag in this.tags) {
+        const tagValues = trackTags.map(track => track[tag]);
+        this.tags[tag].value = tagValues.reduce((a, b) => a === b ? a : "<< multiple >>");
+      }
     }
   }
 }
